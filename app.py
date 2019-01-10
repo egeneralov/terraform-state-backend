324e4a10b8270d04ccc052cd5b3a244dadf7d083
egeneralov <egeneralov@technotimes.org>
2019-01-10 17:01:21 +0300

    5000

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def save_config():
     f.write('config = ' + str(data))
   return str(data)
 
-
-app.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000, debug=True)
